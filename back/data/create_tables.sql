957e195538aa073cfb3fd3cd2eeab9068a09a85e
Antoine Quelin <antoine.quelin@gmail.com>
2022-10-28 11:59:54 +0200

    database OK

--- back/data/create_tables.sql
+++ back/data/create_tables.sql
@@ -5,9 +5,9 @@ DROP TABLE IF EXISTS "wine", "user", "style", "dish", "region", "grapevariety", 
 
 /*CREATE TABLES */
 
--- ----------------------------
--- TABLE region : Create before "wine" because wine has "region" foreign key inside
--- -----------------------------
+/*
+TABLE region : Create before "wine" because wine has "region" foreign key inside
+*/
 CREATE TABLE IF NOT EXISTS "region"(
     "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
     "name" TEXT NOT NULL, 
@@ -15,42 +15,41 @@ CREATE TABLE IF NOT EXISTS "region"(
     "updated_at" TIMESTAMPTZ
 ); 
 
--- ----------------------------
--- TABLE winemaker : Create before "wine" because wine has "winemaker" foreign key inside
--- -----------------------------
+/*
+TABLE winemaker : Create before "wine" because wine has "winemaker" foreign key inside
+*/
 CREATE TABLE IF NOT EXISTS "winemaker"(
     "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     "name" TEXT NOT NULL,
     "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(), 
-    "updated_at" TIMESTAMPTZ 
+    "updated_at" TIMESTAMPTZ
 );
 
--- ----------------------------
--- TABLE wine
--- -----------------------------
+/*
+TABLE wine
+*/
 CREATE TABLE IF NOT EXISTS "wine"(
     "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
     "name" TEXT NOT NULL, 
     "description" TEXT NOT NULL, 
     "appellation" TEXT NOT NULL, 
     "size" TEXT NOT NULL, 
-    "price" NUMERIC('4,2') NOT NULL, 
-    "alcohol" NUMERIC ('3,1') NOT NULL, 
+    "price" DECIMAL NOT NULL, 
+    "alcohol" DECIMAL NOT NULL, 
     "vintage" INT NOT NULL, 
     "color" TEXT NOT NULL, 
     "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(), 
-    "updated_at" TIMESTAMPTZ, 
-    "region_id" INT NOT NULL REFERENCES "region"("id") ON DELETE CASCADE --when we delete region, we delete all associated wines
+    "region_id" INT NOT NULL REFERENCES "region"("id") ON DELETE CASCADE, --when we delete region, we delete all associated wines
     "winemaker_id" INT NOT NULL REFERENCES "winemaker"("id") ON DELETE CASCADE --when we delete winemaker, we delete all associated wines
 );
 
--- ----------------------------
--- TABLE user
--- -----------------------------
+/*
+TABLE user
+*/
 CREATE TABLE IF NOT EXISTS "user"(
     "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     "email" TEXT NOT NULL,  
-    "fistname" TEXT NOT NULL,
+    "firstname" TEXT NOT NULL,
     "lastname" TEXT NOT NULL, 
     "password" TEXT NOT NULL, 
     "avatar" TEXT NULL, 
@@ -60,62 +59,62 @@ CREATE TABLE IF NOT EXISTS "user"(
     "address_postal" INT NULL, 
     "address_city" INT NULL, 
     "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(), 
-    "updated_at" TIMESTAMPTZ, 
+    "updated_at" TIMESTAMPTZ
 );
 
--- ----------------------------
--- TABLE style
--- -----------------------------
+/*
+TABLE style
+*/
 CREATE TABLE IF NOT EXISTS "style"(
     "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
     "name" TEXT NOT NULL,
     "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(), 
-    "updated_at" TIMESTAMPTZ 
+    "updated_at" TIMESTAMPTZ
 );
 
--- ----------------------------
--- TABLE culture
--- -----------------------------
+/*
+TABLE culture
+*/
 CREATE TABLE IF NOT EXISTS "culture"(
     "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
     "name" TEXT NOT NULL,
     "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(), 
-    "updated_at" TIMESTAMPTZ 
+    "updated_at" TIMESTAMPTZ
 );
 
--- ----------------------------
--- TABLE grapevariety
--- -----------------------------
+/*
+TABLE grapevariety
+*/
 CREATE TABLE IF NOT EXISTS "grapevariety"(
     "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
     "name" TEXT NOT NULL,
     "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(), 
-    "updated_at" TIMESTAMPTZ 
+    "updated_at" TIMESTAMPTZ
 );
 
--- ----------------------------
--- TABLE cart
--- -----------------------------
+/*
+TABLE cart
+*/
 CREATE TABLE IF NOT EXISTS "cart"(
     "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
     "user_id" INT NOT NULL REFERENCES "user"("id") ON DELETE CASCADE, -- When delete a user, we delete all associated carts
     "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(), 
-    "updated_at" TIMESTAMPTZ 
+    "updated_at" TIMESTAMPTZ
 );
 
--- ----------------------------
--- TABLE dish
--- -----------------------------
+/*
+TABLE dish
+*/
 CREATE TABLE IF NOT EXISTS "dish"(
     "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
     "name" TEXT NOT NULL,
     "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(), 
-    "upadated_at" TIMESTAMPTZ 
+    "upadated_at" TIMESTAMPTZ
 );
 
--- ----------------------------
--- ASSOCIATION TABLES
--- -----------------------------
+/*
+ASSOCIATION TABLES
+*/
 
 
 CREATE TABLE IF NOT EXISTS "compose"(
@@ -140,7 +139,7 @@ CREATE TABLE IF NOT EXISTS "eat_with"(
 ); 
 
 CREATE TABLE IF NOT EXISTS "cultivate"(
-    "culture_id" INT NOT NULL REFERENCES "cultivate"("id"), 
+    "culture_id" INT NOT NULL REFERENCES "culture"("id"), 
     "wine_id" INT NOT NULL REFERENCES "wine"("id"), 
     "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(), 
     PRIMARY KEY ("culture_id", "wine_id")
@@ -148,10 +147,11 @@ CREATE TABLE IF NOT EXISTS "cultivate"(
 
 CREATE TABLE IF NOT EXISTS "concerns"(
     "quantity" INT NULL, 
-    "cart_id" INT NOT NULL REFERENCES "cultivate"("id") ON DELETE CASCADE, 
+    "cart_id" INT NOT NULL REFERENCES "cart"("id") ON DELETE CASCADE, 
     "wine_id" INT NOT NULL REFERENCES "wine"("id") ON DELETE CASCADE, 
     "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(), 
     PRIMARY KEY ("cart_id", "wine_id")
 ); 
+
 
 COMMIT;
