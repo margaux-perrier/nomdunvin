f392b8456035cead535c81656eca1276c00aefba
margaux-perrier <perriermargaux@gmail.com>
2022-10-31 13:53:20 +0100

    admin add,update,delete wine

--- back/app/router.js
+++ back/app/router.js
@@ -2,10 +2,14 @@ const {Router} = require('express');
 const router = Router(); 
 
 const wineController = require('./controller/wineController'); 
+const adminController = require('./controller/adminController'); 
 
 
 router.get('/', wineController.getAllWines); 
 router.get('/wine/:id', wineController.getOneWineById);
 
+router.post('/wine', adminController.createWine);
+router.patch('/wine/:id', adminController.updateWineById);
+router.delete('/wine/:id', adminController.deleteWineById);
 
 module.exports = router;
