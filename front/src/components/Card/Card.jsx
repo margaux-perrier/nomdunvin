65469fc69f1d434455e6b531e75507b5e6b0ee51
Cyrille <cyrillelegoic@gmail.com>
2022-11-01 03:07:29 +0100

    fix problem with space in tags

--- front/src/components/Card/Card.jsx
+++ front/src/components/Card/Card.jsx
@@ -1,18 +1,22 @@
 // import react
 import React, { Fragment } from 'react';
+// import de Link 
+import { Link } from 'react-router-dom';
+// import logo cart
+import cart from './cart.png';
 // import Scss
 import './card.scss';
 // Component Filter
 function Card({
     id,
-    size, 
-    color, 
-    alcohol, 
-    price, 
-    name, 
-    winemaker, 
-    culture, 
-    region, 
+    size,
+    color,
+    alcohol,
+    price,
+    name,
+    winemaker,
+    culture,
+    region,
     img,
     handleClick
 }) {
@@ -20,18 +24,23 @@ function Card({
         <Fragment>
             <div className="card">
                 <div className='visual'>
-                    <div className="format">
+                    <div className="left-card">
                         <p>{size}</p>
                         <p className='degrees'>{`${alcohol}`}%.vol</p>
                     </div>
                     <div className="card-img">
                         <img className="wine-logo" src={img} alt="red-wine" />
                     </div>
-                    <ul className="card-tag">
-                        {culture.map((item, index) => (
-                        <li key={index} className={`tag-${item}`}>{item}</li>
-                        ))}
-                    </ul>
+                    <div className="right-card">
+                        <ul className="card-tag">
+                            {culture.map((item, index) => (
+                                <li key={index} className={`tag-${item}`}>{item}</li>
+                            ))}
+                        </ul>
+                        <div>
+                            <Link to="/"> <img className="logo-cart" src={cart} alt="cart" /> </Link>
+                        </div>
+                    </div>
                 </div>
                 <div className="card-content">
                     <h2 className="winemaker">{winemaker}</h2>
@@ -39,7 +48,7 @@ function Card({
                     <p className="wine-region">{region}</p>
                     <p className={`tablet-color-${color}`}>•</p>
                 </div>
-                
+
                 <div className="card-price">
                     <button className="price-btn" id={id} onClick={handleClick}>Voir le produit</button>
                     <p className='price'>{price} €</p>
