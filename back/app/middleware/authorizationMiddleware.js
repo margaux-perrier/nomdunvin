d187c56143d9f75014a367b2191479ea818a9544
margaux-perrier <perriermargaux@gmail.com>
2022-11-07 20:56:40 +0100

    middleware JWT token ok

--- back/app/middleware/authorizationMiddleware.js
+++ back/app/middleware/authorizationMiddleware.js
@@ -4,7 +4,7 @@ const jwtSecret = process.env.JWT_SECRET;
 const authorizationMiddleware = (req,res,next) => {
 	try {
 		const token = req.headers.authorization.split(' ')[1];
-		jwt.verify(token, jwtSecret);
+		req.token = jwt.verify(token, jwtSecret);
 		next();
 	} catch (error) {
 		res.status(401).json({ message : 'Invalid authentification token'});
